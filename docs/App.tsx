9d94e28269c4301ffbc7ccc0839998295e539f1a
JounQin <admin@1stg.me>
2025-06-04 22:26:21 +0800

    feat: let's fork and release

---
+++ docs/App.tsx
@@ -0,0 +1,36 @@
+import { lazy, Suspense } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import './global.css'
+import 'github-markdown-css'
+
+const Readme_ = lazy(() => import('../README.md'))
+
+const Readme = () => (
+  <Suspense>
+    <Readme_ />
+  </Suspense>
+)
+
+const Changelog_ = lazy(() => import('../CHANGELOG.md'))
+
+const Changelog = () => (
+  <Suspense>
+    <Changelog_ />
+  </Suspense>
+)
+
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        path="/CHANGELOG.md"
+        element={<Changelog />}
+      ></Route>
+      <Route
+        path="/"
+        element={<Readme />}
+      ></Route>
+    </Routes>
+  </Router>
+)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Use WeakMap to store the object-key mapping so the objects can still be
-// garbage collected. WeakMap uses a hashtable under the hood, so the lookup
+// garbage collected. WeakMap uses a hash table under the hood, so the lookup
 // complexity is almost O(1).
 const table = new WeakMap<object, string>()
 
@@ -14,46 +14,61 @@ let counter = 0
 //
 // This is not a serialization function, and the result is not guaranteed to be
 // parsable.
-export function stableHash(arg: any): string {
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export function stableHash(arg: unknown): string {
   const type = typeof arg
-  const constructor = arg && arg.constructor
+  const constructor = arg?.constructor
   const isDate = constructor === Date
 
   if (Object(arg) === arg && !isDate && constructor != RegExp) {
+    const arg_ = arg as object
     // Object/function, not null/date/regexp. Use WeakMap to store the id first.
     // If it's already hashed, directly return the result.
-    let result = table.get(arg)
-    if (result) return result
+    let result = table.get(arg_)
+    if (result) {
+      return result
+    }
     // Store the hash first for circular reference detection before entering the
     // recursive `stableHash` calls.
     // For other objects like set and map, we use this id directly as the hash.
-    result = ++counter + "~"
-    table.set(arg, result)
-    let index: any
-
+    result = ++counter + '~'
+    table.set(arg_, result)
+    let index: number | string | undefined
     if (constructor === Array) {
+      const arg_ = arg as unknown[]
       // Array.
-      result = "@"
-      for (index = 0; index < arg.length; index++) {
-        result += stableHash(arg[index]) + ","
+      result = '@'
+      for (index = 0; index < arg_.length; index++) {
+        result += stableHash(arg_[index]) + ','
       }
-      table.set(arg, result)
+      table.set(arg_, result)
     } else if (constructor === Object) {
       // Object, sort keys.
-      result = "#"
-      const keys = Object.keys(arg).sort()
-      while ((index = keys.pop() as string) !== undefined) {
-        if (arg[index] !== undefined) {
-          result += index + ":" + stableHash(arg[index]) + ","
+      result = '#'
+      // eslint-disable-next-line sonarjs/no-alphabetical-sort
+      const keys = Object.keys(arg_).sort()
+      while ((index = keys.pop()) !== undefined) {
+        const index_ = index as keyof typeof arg_
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (arg_[index_] !== undefined) {
+          result += index + ':' + stableHash(arg_[index_]) + ','
         }
       }
-      table.set(arg, result)
+      table.set(arg_, result)
     }
     return result
   }
-  if (isDate) return arg.toJSON()
-  if (type === "symbol") return arg.toString()
-  return type === "string" ? JSON.stringify(arg) : "" + arg
+  if (isDate) {
+    return (arg as Date).toJSON()
+  }
+  if (type === 'symbol') {
+    return (arg as symbol).toString()
+  }
+
+  return type === 'string'
+    ? JSON.stringify(arg)
+    : // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      '' + arg
 }
 
-export default stableHash
+export { stableHash as hash }
